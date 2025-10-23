9a4a36228620db19dc7e4bc029cd9d03b66b6da9
renardeinside <polarpersonal@gmail.com>
2025-10-23 13:33:48 +0200

    rework apx plugin

--- hooks/pack.py
+++ hooks/pack.py
@@ -9,11 +9,11 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class PackHook(BuildHookInterface):
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
-        dist_path = Path("src/apx/dist")
-        self.app.display_info(f"Packing {version} into {dist_path.resolve()}")
+        filename = Path("src/apx/dist") / f"apx-plugin.tgz"
+        self.app.display_info(f"Packing {version} into {filename.resolve()}")
         # run bun run build in the project root
         subprocess.run(["bun", "run", "build"])
         # run bun pm pack in the project root
-        subprocess.run(["bun", "pm", "pack", "--destination", dist_path.resolve()])
+        subprocess.run(["bun", "pm", "pack", "--filename", str(filename.resolve())])
 
-        self.app.display_info(f"Packed plugin into {dist_path.resolve()}")
+        self.app.display_info(f"Packed plugin into {filename.resolve()}")
